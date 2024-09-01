5063999a28c6b3b42204ba6123eef3d01e7a20bf
scttgs0 <scttgs0@gmail.com>
2024-08-31 20:15:29 -0500

    remove obsolete platform files

--- make_foenix.sh
+++
@@ -1,9 +0,0 @@
-
-64tass  --m65816 \
-        --long-address \
-        --flat \
-        --nostart \
-        -o claimjumper.pgx \
-        --list=claimjumper.lst \
-        --labels=claimjumper.lbl \
-        claimjumper.asm
